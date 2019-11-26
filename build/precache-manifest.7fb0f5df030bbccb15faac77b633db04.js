c0cd37c7ed33169db363fa74466426a7ba6a0af6
Ankit Curchorcar <ankit.curchorcar@gmail.com>
2019-11-26 15:21:23 -0500

    made build folder available

---
+++ build/precache-manifest.7fb0f5df030bbccb15faac77b633db04.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17bb7681cc948ff92323a0c09a7ac903",
+    "url": "/sternfolio/index.html"
+  },
+  {
+    "revision": "d5dee50f306f5f2b56aa",
+    "url": "/sternfolio/static/css/main.213bfceb.chunk.css"
+  },
+  {
+    "revision": "5e6c0ffe63d40eabaaed",
+    "url": "/sternfolio/static/js/2.ae4018a2.chunk.js"
+  },
+  {
+    "revision": "d5dee50f306f5f2b56aa",
+    "url": "/sternfolio/static/js/main.c8f99ee9.chunk.js"
+  },
+  {
+    "revision": "e2e3d2a168e25b030529",
+    "url": "/sternfolio/static/js/runtime-main.1915c3a6.js"
+  }
+]);
